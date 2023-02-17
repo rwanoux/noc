fd90ee3b9388d2847de74294c8415ff1009c1d65
rwanoux <erwan_lemaire@yahoo.fr>
2023-02-17 11:21:20 +0100

    adding auto refresh when templates are saved with browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,31 @@
 
 const gulp = require('gulp');
 const sass = require('gulp-sass')(require('sass'));
+var browserSync = require('browser-sync').create();
 
 function buildStyles() {
     return gulp.src('./scss/noc.scss')
         .pipe(sass({ outputStyle: 'compressed' }).on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
+        .pipe(gulp.dest('./css'))
+        .pipe(browserSync.stream());
+
+
 };
 
 exports.buildStyles = buildStyles;
 exports.watch = function () {
-    gulp.watch(['./scss/**/*.scss', '.scss/*.scss'], buildStyles)
+    browserSync.init(
+        {
+            server: false,
+            proxy: {
+                target: "localhost:16003",
+                ws: true,
+            }
+
+        }
+    );
+    gulp.watch("./templates/**/*.html").on('change', browserSync.reload);
+
+    gulp.watch(['./scss/**/*.scss', './scss/*.scss'], buildStyles);
+
 };
