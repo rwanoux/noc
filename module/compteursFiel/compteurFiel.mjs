9cc8ae90f42d78ef328d39034746f60bec2626c0
rwanoux <erwan_lemaire@yahoo.fr>
2023-04-06 09:29:21 +0200

    ajout icones + fixe position compteur fiel si video active gauche

--- module/compteursFiel/compteurFiel.mjs
+++ module/compteursFiel/compteurFiel.mjs
@@ -68,7 +68,7 @@ export default class CompteurFiel extends FormApplication {
 
     }
     _injectHTML(html) {
-        html.insertBefore('#players');
+        html.insertAfter('#controls');
         this._element = html;
         html.hide().fadeIn(200);
     }
