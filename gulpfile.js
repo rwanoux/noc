b193f1ad9b2b674bd542d03502b2f3299d7eff98
rwanoux <erwan_lemaire@yahoo.fr>
2024-07-16 10:35:09 +0200

    fixe gulp et sidebar nav

--- gulpfile.js
+++ gulpfile.js
@@ -12,8 +12,14 @@ function buildStyles() {
 
 
 };
-function reloadTemplates() {
+function reloadTemplatesHTML() {
+
     return browserSync.reload("templates/**/*.html")
+}
+
+function reloadTemplatesHBS() {
+
+    return browserSync.reload("templates/**/*.hbs")
 }
 exports.buildStyles = buildStyles;
 exports.watch = function () {
@@ -21,14 +27,14 @@ exports.watch = function () {
         {
             server: false,
             proxy: {
-                target: "https://localhost:443/",
+                target: "https://localhost:30000/",
                 ws: true,
             }
 
         }
     );
-    gulp.watch("./templates/**/*.html").on('change', reloadTemplates);
-
+    gulp.watch("./templates/**/*.html").on('change', reloadTemplatesHTML);
+    gulp.watch("./templates/**/*.hbs").on('change', reloadTemplatesHBS);
     gulp.watch(['./scss/**/*.scss', './scss/*.scss'], buildStyles);
 
 };
